2db05d06916b1b571ee173d8379a8f51948a8853
Emad <syed.emadmoeez@gmail.com>
2025-09-13 18:18:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // 10 media items: 5 videos + 5 images, using media itself as grid thumbnail
 const MEDIA = [
-  { type: "video", src: "movie.mov", title: "Video Memory 1" },
+  { type: "video", src: "movie.mp4", title: "Video Memory 1" },
   { type: "image", src: "bhoot.jpeg", title: "Photo Memory 1" },
-  { type: "video", src: "movie2.mov", title: "Video Memory 2" },
+  { type: "video", src: "movie2.mp4", title: "Video Memory 2" },
   { type: "image", src: "dii.jpg", title: "Photo Memory 2" },
-  { type: "video", src: "movie3.mov", title: "Video Memory 3" },
+  { type: "video", src: "movie3.mp4", title: "Video Memory 3" },
   { type: "image", src: "dii2.jpg", title: "Photo Memory 3" },
-  { type: "video", src: "movie4.mov", title: "Video Memory 4" },
+  { type: "video", src: "movie4.mp4", title: "Video Memory 4" },
   { type: "image", src: "dii3.jpeg", title: "Photo Memory 4" },
-  { type: "video", src: "movie5.mov", title: "Video Memory 5" },
+  { type: "video", src: "movie5.mp4", title: "Video Memory 5" },
   { type: "image", src: "dii4.jpeg", title: "Photo Memory 5" },
 ];
 
